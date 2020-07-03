8adafbee2d7bc28e29e904cc4b8e5863f7ef4bc4
lucaspere <lucasrupestre@hotmail.com>
2020-07-02 21:39:34 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+   s3: {
+     REGION: "us-east-02",
+     BUCKET: "lucas-notes-app-uploads"
+   },
+   apiGateway: {
+     REGION: "us-east-02",
+     URL: "https://fz2wai1psj.execute-api.us-east-2.amazonaws.com/dev"
+   },
+   cognito: {
+     REGION: "us-east-2",
+     USER_POOL_ID: "us-east-2_gAsrnMpQ7",
+     APP_CLIENT_ID: "43fb80pulk5d2c0m7sgnecb12f",
+     IDENTITY_POOL_ID: "us-east-2:df2c7381-1439-4b99-91eb-cc5a6b7428df"
+
+   }
+ };
